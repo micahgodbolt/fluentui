7b3faf691698b13a65b1964e7c7b616f712915ce
Micah Godbolt <mgodbolt@microsoft.com>
2022-11-07 15:57:42 -0800

    stress with vars to start

--- apps/stress-test/src/pages/react/global-vars/index.tsx
+++ apps/stress-test/src/pages/react/global-vars/index.tsx
@@ -3,30 +3,29 @@ import * as ReactDOM from 'react-dom';
 import { getTestOptions } from '../../../shared/utils/testOptions';
 import { ReactTest } from '../../../shared/react/ReactTest';
 import { makeStaticStyles } from '@griffel/react';
-import { num } from '../../../consts';
 
 const { fixtureName, rendererName, r } = getTestOptions();
-const itemsArray = [...Array(num).keys()];
+const itemsArray = [...Array(20).keys()];
 const myStyles = itemsArray.reduce(
   (acc, n) => {
     acc.body = {
       ...(acc.body || {}),
-      [`--color-${n}`]: `rgb(255,${n},0)`,
-      [`--backgroundColor-${n}`]: `rgb(${n * 10},${150 - n * 5},0)`,
-      [`--fontSize-${n}`]: `${4 + n}px`,
-      [`--lineHeight-${n}`]: `${12 + n}px`,
-      [`--marginTop-${n}`]: `${1 + n}px`,
-      [`--marginBottom-${n}`]: `${2 + n}px`,
-      [`--marginLeft-${n}`]: `${3 + n}px`,
-      [`--marginRight-${n}`]: `${4 + n}px`,
-      [`--paddingTop-${n}`]: `${1 + n}px`,
-      [`--paddingBottom-${n}`]: `${2 + n}px`,
-      [`--paddingLeft-${n}`]: `${3 + n}px`,
-      [`--paddingRight-${n}`]: `${4 + n}px`,
-      [`--top-${n}`]: `${1 + n}px`,
-      [`--bottom-${n}`]: `${2 + n}px`,
-      [`--left-${n}`]: `${3 + n}px`,
-      [`--right-${n}`]: `${4 + n}px`,
+      [`--color-global-${n}`]: `rgb(255,${n},0)`,
+      [`--bg-global-${n}`]: `rgb(${n * 10},${150 - n * 5},0)`,
+      [`--fontSize-global-${n}`]: `${4 + n}px`,
+      [`--lh-global-${n}`]: `${12 + n}px`,
+      [`--mt-global-${n}`]: `${1 + n}px`,
+      [`--mb-global-${n}`]: `${2 + n}px`,
+      [`--ml-global-${n}`]: `${3 + n}px`,
+      [`--mr-global-${n}`]: `${4 + n}px`,
+      [`--pt-global-${n}`]: `${1 + n}px`,
+      [`--pb-global-${n}`]: `${2 + n}px`,
+      // [`--paddingLeft-${n}`]: `${3 + n}px`,
+      // [`--paddingRight-${n}`]: `${4 + n}px`,
+      // [`--top-${n}`]: `${1 + n}px`,
+      // [`--bottom-${n}`]: `${2 + n}px`,
+      // [`--left-${n}`]: `${3 + n}px`,
+      // [`--right-${n}`]: `${4 + n}px`,
     };
     return acc;
   },
--- apps/stress-test/src/renderers/react/utils/getMyStyles.ts
+++ apps/stress-test/src/renderers/react/utils/getMyStyles.ts
@@ -7,29 +7,29 @@ export const getMyStyles = (vars: number = 1, items: number = 20) => {
       const cutoff = (vars - item) / 10;
 
       acc[`inner${item}`] = {
-        color: cutoff > 0 ? `var(--color-${item})` : `rgb(255,${item},0)`,
-        backgroundColor: cutoff > 1 ? `var(--backgroundColor-${item})` : `rgb(${item * 1},${150 - item * 1},0)`,
-        fontSize: cutoff > 2 ? `var(--fontSize-${item})` : `${8 + item / 2}px`,
-        lineHeight: cutoff > 3 ? `var(--lineHeight-${item})` : `${12 + item}px`,
-        marginTop: cutoff > 4 ? `var(--marginTop-${item})` : `${1 + item}px`,
-        marginBottom: cutoff > 5 ? `var(--marginBottom-${item})` : `${2 + item}px`,
-        marginLeft: cutoff > 6 ? `var(--marginLeft-${item})` : `${3 + item}px`,
-        marginRight: cutoff > 7 ? `var(--marginRight-${item})` : `${4 + item}px`,
-        paddingTop: cutoff > 8 ? `var(--paddingTop-${item})` : `${1 + item}px`,
-        paddingBottom: cutoff > 9 ? `var(--paddingBottom-${item})` : `${2 + item}px`,
+        color: cutoff > 0 ? `var(--color-${item}, --color-global-${item})` : `var( --color-global-${item})`,
+        bg: cutoff > 1 ? `var(--bg-${item}, --bg-global-${item})` : `var( --bg-global-${item})`,
+        fontSize: cutoff > 2 ? `var(--fontSize-${item}, --fontSize-global-${item})` : `var( --fontSize-global-${item})`,
+        lineHeight: cutoff > 3 ? `var(--lh-${item}, --lh-global-${item})` : `var( --lh-global-${item})`,
+        marginTop: cutoff > 4 ? `var(--mt-${item}, --mt-global-${item})` : `var( --mt-global-${item})`,
+        marginBottom: cutoff > 5 ? `var(--mb-${item}, --mb-global-${item})` : `var( --mb-global-${item})`,
+        marginLeft: cutoff > 6 ? `var(--ml-${item}, --ml-global-${item})` : `var( --ml-global-${item})`,
+        marginRight: cutoff > 7 ? `var(--mr-${item}, --mr-global-${item})` : `var( --mr-global-${item})`,
+        paddingTop: cutoff > 8 ? `var(--pt-${item}, --pt-global-${item})` : `var( --pt-global-${item})`,
+        paddingBottom: cutoff > 9 ? `var(--pb-${item}, --pb-global-${item})` : `var( --pb-global-${item})`,
       };
 
       const baseOuterStyles = {
         [`--color-${item}`]: `rgb(255,${item}, ${random()})`,
-        [`--backgroundColor-${item}`]: `rgb(${item * 1},${random()},${random()})})`,
+        [`--bg-${item}`]: `rgb(${item * 1},${random()},${random()})})`,
         [`--fontSize-${item}`]: `${8 + item / 2}px`,
-        [`--lineHeight-${item}`]: `${12 + item}px`,
-        [`--marginTop-${item}`]: `${1 + item}px`,
-        [`--marginBottom-${item}`]: `${2 + item}px`,
-        [`--marginLeft-${item}`]: `${3 + item}px`,
-        [`--marginRight-${item}`]: `${4 + item}px`,
-        [`--paddingTop-${item}`]: `${1 + item}px`,
-        [`--paddingBottom-${item}`]: `${2 + item}px`,
+        [`--lh-${item}`]: `${12 + item}px`,
+        [`--mt-${item}`]: `${1 + item}px`,
+        [`--mb-${item}`]: `${2 + item}px`,
+        [`--ml-${item}`]: `${3 + item}px`,
+        [`--mr-${item}`]: `${4 + item}px`,
+        [`--pt-${item}`]: `${1 + item}px`,
+        [`--pb-${item}`]: `${2 + item}px`,
       };
 
       // 34
